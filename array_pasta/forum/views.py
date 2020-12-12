6fc31484bcfbf261598a7dbc7444bc31e8ddd950
Andresjm30 <andresjm30@gmail.com>
2020-12-12 10:05:37 -0500

    Deleted unnecessary files

--- array_pasta/forum/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'forum/index.html')
-
-def about_us(request):
-    return render(request, 'forum/about_us.html')
